41dafd92b3fd8206e98c38d6cb9de4a9066ce384
Joey <xjoey96@gmail.com>
2022-09-14 15:49:49 -0500

    basename

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,7 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="https://joey-red.github.io/EventPlanner/">
+      <BrowserRouter basename="/EventPlanner/">
         <FrontRouter />
       </BrowserRouter>
     </Provider>
