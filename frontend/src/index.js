a877370d6c2b60ffec665571ea87ddb94cd5b5b8
Joey <xjoey96@gmail.com>
2022-09-15 03:46:44 -0500

    removed default value

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,7 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename="/EventPlanner/" hashType="slash">
+      <HashRouter basename="/EventPlanner/">
         <FrontRouter />
       </HashRouter>
     </Provider>
